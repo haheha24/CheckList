8af458eb01d6bf5c0da38a70c207bcb88318339a
haheha24 <haheha24@users.noreply.github.com>
2021-03-12 01:08:34 +1100

    12.3.21 Responsive @600
    Made a responsive layout at 600px.
    
    Added a 75% width to newCheckListBox.
    Also neatened up code.
    
    Added a note to improve flex layout for .showGrid-item class.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function newChecklist() {
-  document.getElementById("newChecklistbox").classList.toggle("showGrid");
+// New Checklist Button
+function newCheckList() {
+  document.getElementById("newCheckListBox").classList.toggle("showGrid");
+}
+      // Add an event listener to hide newChecklistbox when mouse is clicked
+      // outside of the box.
+
+// Add a new Check list item under Check List Panel inside div checkList
+function newCheckListItem() {
+//  document.getElementById("unlistedCheckList").
 }
