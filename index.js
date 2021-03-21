f1abbc194755bb6792e5484ed4ede177260a4ac0
haheha24 <haheha24@users.noreply.github.com>
2021-03-22 00:22:11 +1100

    Revert "basic implementation / half finished"
    This reverts commit b8d673999f402739c373b618c8cd64d816954ccb.

--- index.js
+++ index.js
@@ -2,28 +2,10 @@
 function newCheckList() {
   document.getElementById("newCheckListBox").classList.toggle("showGrid");
 }
-// function hideNewCheckList() {
-//  document.getElementById("newCheckListBox").addEventListener("click", myFunction);
-// }
       // Add an event listener to hide newChecklistbox when mouse is clicked
       // outside of the box.
 
 // Add a new Check list item under Check List Panel inside div checkList
-// var inputEvent = input.addEventListener
 function newCheckListItem() {
-  let checkListName = document.getElementById("checkListTxtBoxID").value;
-  let checkUl = document.getElementById("checkListUl");
-  let checkLi = document.createElement("li");
-  let checkA = document.createElement("a");
-  let checkRegExp = (/\s/); // Create an regexp array that covers whitespace and
-                            // no repeating checkListNames
-  if (checkListName === "" || checkRegExp.exec(checkListName) !== null) {
-    window.alert("Please choose another name")
-  } else {
-      checkUl.appendChild(checkLi);
-      checkLi.classList.add("checkLi");
-      checkLi.appendChild(checkA);
-      checkA.innerHTML = checkListName;
-      // set the attribute of checkA to whatever the main content is dynamically
-  }
+//  document.getElementById("unlistedCheckList").
 }
