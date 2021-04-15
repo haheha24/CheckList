b8ae387896fc443a49c6d1935513485923f20fe7
haheha24 <haheha24@users.noreply.github.com>
2021-04-16 01:34:14 +1000

    Minor adjustments to html markup. Minor adjustments to CSS. Major improvement in Javascript. Cleaned up and formatted index.js, as well as adding highlighting functionality to new checklists created under the left panel.

--- index.js
+++ index.js
@@ -1,53 +1,74 @@
+// LocalStorage
+var checkLocStor = localStorage;
+
 // New Checklist Button
 function newCheckList() {
   document.getElementById("newCheckListBox").classList.toggle("showGrid");
 }
-// function hideNewCheckList() {
-//  document.getElementById("newCheckListBox").addEventListener("click", myFunction);
-// }
-      // Add an event listener to hide newChecklistbox when mouse is clicked
-      // outside of the box.
-
-// Add a new Check list item under Check List Panel inside div checkList
-// var inputEvent = input.addEventListener
-
+// Add an event listener to hide newChecklistbox when mouse is clicked
+// outside of the box.
 
 //Load checkListArray local storage and print array in check list panel - check list.
 var checkListArray = [];
-
-//
 function newCheckListItem() {
   //set up the conditions of what the check list name can be
   let checkListName = document.getElementById("checkListTxtBoxID").value;
-  let checkRegExp = (/\s/);
-  let conditionalOne = checkListName === "" || checkRegExp.exec(checkListName) !== null; // Create an regexp array that covers whitespace and no repeating checkListNames
-  let conditionalTwo = checkListArray.includes(checkListName);  
+  let checkRegExp = /\s/;
+  let conditionalOne =
+    checkListName === "" || checkRegExp.exec(checkListName) !== null; // Create an regexp array that covers whitespace and no repeating checkListNames
+  let conditionalTwo = checkListArray.includes(checkListName);
   //DOM requirements
   let checkUl = document.getElementById("checkListUl");
   let checkLi = document.createElement("li");
-  let checkA = document.createElement("a");
   //begin the actual function
   if (conditionalOne) {
-    window.alert("Please choose another name. Only complete words with no spaces are accepted.")
+    window.alert(
+      "Please choose another name. Only complete words with no spaces are accepted."
+    );
   } else if (conditionalTwo) {
-    window.alert("There is already a list with that name")
+    window.alert(
+      "There is already a list with that name. Please choose another."
+    );
   } else {
     checkListArray.push(checkListName);
-    console.log(checkListArray);
     checkUl.appendChild(checkLi);
     checkLi.classList.add("checkLi");
-    checkLi.appendChild(checkA);
-    checkA.innerHTML = checkListName;
-    checkA.setAttribute("href", "#");
-    
-    //clear the contents of the checkListTxtBox
+    checkLi.innerText = checkListName;
+    //"show" the currently highlighted checkList
+    if (checkListArray.length > 1) {
+      let checkListId = document.getElementById("checkListUl");
+      let checkHighlight = checkListId.getElementsByClassName("checkLi");
+      let checkLength = checkListArray.length - 1;
+      for (let i = 0; i < checkHighlight.length; i++) {
+        checkHighlight[i].classList.remove("currentList");
+      }
+      //getElementsByClassName only shows a list of the matching elements, not the element itself
+      //We use [] to specify which of the matching elements to work on
+      //.length begins from 1, so -1 from checkListArray will match the integer for checkHighLight array.
+      checkHighlight[checkLength].classList.add("currentList");
+    } else {
+      checkLi.classList.add("currentList");
+    }
+    //clear the contents of the checkListTxtBox and hide it again
     document.getElementById("checkListTxtBoxID").value = "";
     document.getElementById("newCheckListBox").classList.remove("showGrid");
-/* 
-          COMMIT AND ADD NOTES ABOUT CONDITIONALS, CHANGES TO THE IF FUNCTIONS, REMOVAL OF TXT AND HIDING THE BOX
-*/
-    // set the attribute of checkA to whatever the main content is dynamically
+    //Call active() function
+    active();
   }
-  var checkLocStor = localStorage;
 }
-
+function active() {
+  let checkListId = document.getElementById("checkListUl");
+  let checkHighlight = checkListId.getElementsByClassName("checkLi");
+  for (let i = 0; i < checkHighlight.length; i++) {
+    checkHighlight[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("currentList");
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(
+          " currentList",
+          ""
+        );
+      }
+      this.className += " currentList";
+    });
+  }
+}
